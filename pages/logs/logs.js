2bdd43bfd28b63cac52a1d060c4123db59a0610f
System Administrator <root@zhaoyimingdeMacBook-Pro.local>
2018-07-17 15:33:43 +0800

    startPullDownRefresh

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -7,8 +7,41 @@ console.log(wxUtil);
 const app = getApp();
 
 Page({
+  data: {
+    byQrcodeInto: false
+  },
+
   onLoad: (options) => {
-		console.log(options);
-		console.log(app.globalData.num);
+		
+  },
+
+  onShow: function ()  {
+    console.log(this);
+    if (wx.getStorageSync('byQrcodeIntoLog')) {
+      this.setData({
+        byQrcodeInto: true
+      });
+    } else {
+      this.setData({
+        byQrcodeInto: false
+      });
+    }
+
+    wx.startPullDownRefresh({
+      success: function (res) {
+        console.log(res);
+        setTimeout(function () {
+          wx.stopPullDownRefresh();
+        }, 3000);
+      }
+    });
+  },
+
+  onPullDownRefresh: function () {
+    console.log('onPullDownRefresh-log');
+  },
+
+  onReachBottom: function () {
+    console.log('onReachBottom-log');
   }
 })
