51a0dc1a8991d8b2feed482829fd12d0842dadb0
zymfe <1047832475@qq.com>
2019-09-29 13:30:09 +0800

    add testing

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    imgArr: [   'https://zhaoyiming-1254801202.cos.ap-beijing.myqcloud.com/WechatIMG37.jpeg'
+    ]
   },
 
   /**
@@ -80,5 +81,18 @@ Page({
 
   toLogin (userInfo) {
     app.getUserInfo(userInfo);
+  },
+
+  previewImg: function (e) {
+    console.log(e.currentTarget.dataset.index);
+    var index = e.currentTarget.dataset.index;
+    var imgArr = this.data.imgArr;
+    wx.previewImage({
+      current: imgArr[index],     //当前图片地址
+      urls: imgArr,               //所有要预览的图片的地址集合 数组形式
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
   }
 })
