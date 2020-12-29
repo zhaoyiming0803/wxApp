d960cd83409fdd0914ba8d353a6bd19683483adb
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2020-12-29 15:32:20 +0800

    test

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -3,19 +3,24 @@ Component({
     selected: 0,
     color: "#7A7E83",
     selectedColor: "#3cc51f",
-    list: [{
-      pagePath: "/pages/index/index",
-      iconPath: "/image/icon_component.png",
-      selectedIconPath: "/image/icon_component_HL.png",
-      text: "组件1"
-    }, {
-      pagePath: "/pages/logs/logs",
-      iconPath: "/image/icon_API.png",
-      selectedIconPath: "/image/icon_API_HL.png",
-      text: "接口2"
-    }]
+    list: []
   },
   attached() {
+    setTimeout(() => {
+      this.setData({
+        list: [{
+          pagePath: "/pages/index/index",
+          iconPath: "/image/icon_component.png",
+          selectedIconPath: "/image/icon_component_HL.png",
+          text: "组件1"
+        }, {
+          pagePath: "/pages/logs/logs",
+          iconPath: "/image/icon_API.png",
+          selectedIconPath: "/image/icon_API_HL.png",
+          text: "接口2"
+        }]
+      })
+    }, 2000)
   },
   methods: {
     switchTab(e) {
