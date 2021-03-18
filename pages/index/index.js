b070f8df880da44ff6b29f6cf2189466530b2b69
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-03-18 17:09:39 +0800

    test

--- pages/index/index.js
+++ pages/index/index.js
@@ -22,6 +22,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    setTimeout(() => {
+      wx.navigateTo({
+        // url: '/pages/progress/progress',
+        url: '../progress/progress'
+      })
+    }, 2000)
+
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
         selected: 0
